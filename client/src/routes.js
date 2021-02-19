2b615324f3db01e3cd5288e3c0c69936c5f6aba3
ary4n99 <ary4n99@gmail.com>
2021-02-19 13:47:19 +0000

    Adds home page

--- client/src/routes.js
+++ client/src/routes.js
@@ -10,6 +10,7 @@ import NotFoundView from 'src/views/errors/NotFoundView';
 import ProductListView from 'src/views/product/ProductListView';
 import RegisterView from 'src/views/auth/RegisterView';
 import SettingsView from 'src/views/settings/SettingsView';
+import HomeView from 'src/views/home/HomeView';
 
 const routes = [
   {
@@ -21,6 +22,7 @@ const routes = [
       { path: 'dashboard', element: <DashboardView /> },
       { path: 'products', element: <ProductListView /> },
       { path: 'settings', element: <SettingsView /> },
+      { path: 'home', element: <HomeView /> },
       { path: '*', element: <Navigate to="/404" /> }
     ]
   },
---
+++ client/src/views/home/HomeView/index.js
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import {
+  Box,
+  Container,
+  Grid,
+  makeStyles
+} from '@material-ui/core';
+import Page from 'src/components/Page';
+import HomeCard from './HomeCard';
+import data from './data';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    backgroundColor: theme.palette.background.dark,
+    minHeight: '100%',
+    paddingBottom: theme.spacing(3),
+    paddingTop: theme.spacing(3)
+  },
+  homeCard: {
+    height: '100%'
+  }
+}));
+
+const Home = () => {
+  const classes = useStyles();
+  const [recipes] = useState(data);
+
+  return (
+    <Page
+      className={classes.root}
+      title="Home"
+    >
+      <Container maxWidth={false}>
+        <Box mt={3}>
+          <Grid
+            container
+            spacing={3}
+          >
+            {recipes.map((recipe) => (
+              <Grid
+                item
+                key={recipe.id}
+                lg={4}
+                md={6}
+                xs={12}
+              >
+                <HomeCard
+                  className={classes.homeCard}
+                  recipe={recipe}
+                />
+              </Grid>
+            ))}
+          </Grid>
+        </Box>
+        <Box
+          mt={3}
+          display="flex"
+          justifyContent="center"
+        >
+        </Box>
+      </Container>
+    </Page>
+  );
+};
+
+export default Home;
