aeafc0a7ef609ac5036e2ab910ff96c73c88ee60
ary4n99 <ary4n99@gmail.com>
2021-02-21 23:29:19 +0000

    Adds dropdown selectors on search page

--- client/src/views/search/SearchView/components/Searchbar.js
+++ client/src/views/search/SearchView/components/Searchbar.js
@@ -3,9 +3,17 @@ import PropTypes from "prop-types";
 import clsx from "clsx";
 import {
   Box,
+  Button,
   TextField,
+  Grid,
   InputAdornment,
   SvgIcon,
+  Select,
+  InputLabel,
+  Input,
+  Checkbox,
+  MenuItem,
+  ListItemText,
   makeStyles,
 } from "@material-ui/core";
 import { Search as SearchIcon } from "react-feather";
@@ -14,28 +22,138 @@ const useStyles = makeStyles((theme) => ({
   root: {},
 }));
 
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
+
+const cuisineNames = [
+  "African",
+  "American",
+  "British",
+  "Cajun",
+  "Caribbean",
+  "Chinese",
+  "Eastern European",
+  "European",
+  "French",
+  "German",
+  "Greek",
+  "Indian",
+  "Irish",
+  "Italian",
+  "Japanese",
+  "Jewish",
+  "Korean",
+  "Latin American",
+  "Mediterranean",
+  "Mexican",
+  "Middle Eastern",
+  "Nordic",
+  "Southern",
+  "Spanish",
+  "Thai",
+  "Vietnamese",
+];
+
+const typeNames = [
+  "Main course",
+  "Side dish",
+  "Dessert",
+  "Appetizer",
+  "Salad",
+  "Bread",
+  "Breakfast",
+  "Soup",
+  "Beverage",
+  "Sauce",
+  "Marinade",
+  "Fingerfood",
+  "Snack",
+  "Drink",
+];
+
 const Searchbar = ({ className, ...rest }) => {
   const classes = useStyles();
+  const [cuisineName, setCuisineName] = React.useState([]);
+  const [typeName, setTypeName] = React.useState([]);
+
+  const handleChangeCuisine = (event) => {
+    setCuisineName(event.target.value);
+  };
+  const handleChangeType = (event) => {
+    setTypeName(event.target.value);
+  };
 
   return (
     <div className={clsx(classes.root, className)} {...rest}>
-      <Box mt={3}>
-        <Box>
-          <TextField
-            fullWidth
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">
-                  <SvgIcon fontSize="small" color="action">
-                    <SearchIcon />
-                  </SvgIcon>
-                </InputAdornment>
-              ),
-            }}
-            placeholder="Search recipes"
-            variant="outlined"
-          />
-        </Box>
+      <Box mt={1}>
+        <Grid container spacing={3}>
+          <Grid item md={6} xs={12}>
+            <TextField
+              fullWidth
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <SvgIcon fontSize="small" color="action">
+                      <SearchIcon />
+                    </SvgIcon>
+                  </InputAdornment>
+                ),
+              }}
+              placeholder="Search recipes"
+              variant="outlined"
+            />
+          </Grid>
+          <Grid item md={3} xs={12}>
+            <InputLabel id="type-label">Type</InputLabel>
+            <Select
+              labelId="type-label"
+              id="type"
+              multiple
+              fullWidth
+              value={typeName}
+              onChange={handleChangeType}
+              input={<Input />}
+              renderValue={(selected) => selected.join(", ")}
+              MenuProps={MenuProps}
+            >
+              {typeNames.map((name) => (
+                <MenuItem key={name} value={name}>
+                  <Checkbox checked={typeName.indexOf(name) > -1} />
+                  <ListItemText primary={name} />
+                </MenuItem>
+              ))}
+            </Select>
+          </Grid>
+          <Grid item md={3} xs={12}>
+            <InputLabel id="cuisine-label">Cuisine</InputLabel>
+            <Select
+              labelId="cuisine-label"
+              id="cuisine"
+              multiple
+              fullWidth
+              value={cuisineName}
+              onChange={handleChangeCuisine}
+              input={<Input />}
+              renderValue={(selected) => selected.join(", ")}
+              MenuProps={MenuProps}
+            >
+              {cuisineNames.map((name) => (
+                <MenuItem key={name} value={name}>
+                  <Checkbox checked={cuisineName.indexOf(name) > -1} />
+                  <ListItemText primary={name} />
+                </MenuItem>
+              ))}
+            </Select>
+          </Grid>
+        </Grid>
       </Box>
     </div>
   );
