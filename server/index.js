d1c9a643b7d02b1f44286dbab57db4136e7833c6
ary4n99 <ary4n99@gmail.com>
2021-02-21 20:38:35 +0000

    Uses bcrypt to hash passwords

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
 const express = require("express");
-const app = express();
-const mysql = require("mysql");
 const cors = require("cors");
+const mysql = require("mysql");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+const port = 3001;
+const app = express();
+
 require("dotenv").config();
 
 app.use(express.json());
@@ -20,15 +24,47 @@ app.post("/register", (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
 
-  con.query(
-    "INSERT INTO users (firstname, lastname, email, password) VALUES (?,?,?,?)",
-    [firstname, lastname, email, password],
-    (err, result) => {
-      console.log(err);
-    }
-  );
+  bcrypt.hash(password, saltRounds, function (err, hash) {
+    console.log(hash);
+    con.query(
+      "INSERT INTO users (firstname, lastname, email, password) VALUES (?,?,?,?)",
+      [firstname, lastname, email, hash],
+      (err, result) => {
+        if (err) {
+          console.log("DB(E): " + err);
+        } else {
+          console.log("DB(R): " + result);
+        }
+      }
+    );
+    console.log("BCRYPT(E): " + err);
+  });
 });
 
-app.listen(3001, () => {
+app.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  con.query(
+    "SELECT * FROM users (email, password) VALUES (?,?)",
+    [email, hash],
+    (err, result) => {
+      if (err) {
+        console.log("DB(E): " + err);
+      } else {
+        console.log("DB(R): " + result);
+      }
+    }
+  );
+
+  bcrypt.compare(password, hash, function (err, res) {
+    if (err) {
+      console.log("BCRYPT(E): " + err);
+    } else {
+      console.log("BCRYPT(R): " + res);
+    }
+  });
+});
+
+app.listen(port, () => {
   console.log("Server is running...");
 });
