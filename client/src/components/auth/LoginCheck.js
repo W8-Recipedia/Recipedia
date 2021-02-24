b623bc462584234ea6fa5a1dba9b01589885fe32
ary4n99 <ary4n99@gmail.com>
2021-02-24 13:18:17 +0000

    Minor improvements to login system

--- client/src/components/auth/LoginCheck.js
+++ client/src/components/auth/LoginCheck.js
@@ -1,24 +1,25 @@
 import { useNavigate } from "react-router-dom";
-import { CircularProgress } from "@material-ui/core";
 import React, { useState } from "react";
 import { getUserCredentials } from "src/components/auth/UserAuth";
 import LandingView from "src/views/landing/LandingView";
 import LoginView from "src/views/login/LoginView";
+import DashboardLayout from "src/dashboard/DashboardLayout";
+
 
 const LoginCheck = () => {
   const navigate = useNavigate();
   const pathname = window.location.pathname;
   const [isloggedIn, setIsLoggedIn] = useState(() => {
     getUserCredentials().then((authResponse) => {
-      console.log(authResponse);
       if (authResponse.data.loggedIn) {
         navigate("/app/home");
-      } else {
+      }
+      else {
         setIsLoggedIn("false");
       }
     });
   });
-  return isloggedIn == "false" ? (pathname == "/" ? <LandingView /> : <LoginView/> ) : <CircularProgress/>;
+  return isloggedIn == "false" ? (pathname == "/login" ? <LoginView/>: <LandingView />  ) : null;
 };
 
 export default LoginCheck;
