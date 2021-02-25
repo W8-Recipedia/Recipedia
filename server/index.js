4a8e0c6f7dea4d3af82d8f3f2ab2f6c72226dbeb
ary4n99 <ary4n99@gmail.com>
2021-02-25 19:25:06 +0000

    Adds CORS origin url as env variables

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: [process.env.CLIENT_URL],
     methods: ["GET", "POST"],
     credentials: true,
   })
