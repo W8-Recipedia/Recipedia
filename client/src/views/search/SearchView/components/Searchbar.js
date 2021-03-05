a06b6c799870b315a1dfedf4ae960e4d53b0f48b
ary4n99 <ary4n99@gmail.com>
2021-03-05 12:33:21 +0000

    Wraps searchbar with card component

--- client/src/views/search/SearchView/components/Searchbar.js
+++ client/src/views/search/SearchView/components/Searchbar.js
@@ -12,7 +12,8 @@ import {
   Checkbox,
   MenuItem,
   ListItemText,
-  Paper
+  Card,
+  CardContent
 } from "@material-ui/core";
 import { Search as SearchIcon } from "react-feather";
 
@@ -102,74 +103,77 @@ const initialFormData = Object.freeze({
   };
 
   return (
-
-      <Box mt={1}>
-        <Grid container spacing={3}>
-          <Grid item md={6} xs={12}>
-            <form noValidate autoComplete="off" onSubmit={onSubmit}>
-              <TextField
-                variant="outlined"
-                name="query"
-                onChange={handleChange}
-                value={formState.query}
-                label="Search recipes"
-                InputProps={{
-                  endAdornment: (
-                    <InputAdornment position="end">
-                      <IconButton type="submit">
-                        <SearchIcon />
-                      </IconButton>
-                    </InputAdornment>
-                  ),
-                }}
+    <Box mt={1}>
+      <Card>
+        <Box p={2}>
+          <Grid container spacing={3}>
+            <Grid item md={6} xs={12}>
+              <form noValidate autoComplete="off" onSubmit={onSubmit}>
+                <TextField
+                  variant="outlined"
+                  name="query"
+                  onChange={handleChange}
+                  value={formState.query}
+                  label="Search recipes"
+                  InputProps={{
+                    endAdornment: (
+                      <InputAdornment position="end">
+                        <IconButton type="submit">
+                          <SearchIcon />
+                        </IconButton>
+                      </InputAdornment>
+                    ),
+                  }}
+                  fullWidth
+                />
+              </form>
+            </Grid>
+            <Grid item md={3} xs={12}>
+              <InputLabel id="type-label">Type</InputLabel>
+              <Select
+                labelId="type-label"
+                id="type"
+                multiple
                 fullWidth
-              />
-            </form>
+                value={typeName}
+                onChange={handleChangeType}
+                input={<Input />}
+                renderValue={(selected) => selected.join(", ")}
+                MenuProps={MenuProps}
+              >
+                {typeNames.map((name) => (
+                  <MenuItem key={name} value={name}>
+                    <Checkbox checked={typeName.indexOf(name) > -1} />
+                    <ListItemText primary={name} />
+                  </MenuItem>
+                ))}
+              </Select>
+            </Grid>
+            <Grid item md={3} xs={12}>
+              <InputLabel id="cuisine-label">Cuisine</InputLabel>
+              <Select
+                labelId="cuisine-label"
+                id="cuisine"
+                multiple
+                fullWidth
+                value={cuisineName}
+                onChange={handleChangeCuisine}
+                input={<Input />}
+                renderValue={(selected) => selected.join(", ")}
+                MenuProps={MenuProps}
+              >
+                {cuisineNames.map((name) => (
+                  <MenuItem key={name} value={name}>
+                    <Checkbox checked={cuisineName.indexOf(name) > -1} />
+                    <ListItemText primary={name} />
+                  </MenuItem>
+                ))}
+              </Select>
+            </Grid>
           </Grid>
-          <Grid item md={3} xs={12}>
-            <InputLabel id="type-label">Type</InputLabel>
-            <Select
-              labelId="type-label"
-              id="type"
-              multiple
-              fullWidth
-              value={typeName}
-              onChange={handleChangeType}
-              input={<Input />}
-              renderValue={(selected) => selected.join(", ")}
-              MenuProps={MenuProps}
-            >
-              {typeNames.map((name) => (
-                <MenuItem key={name} value={name}>
-                  <Checkbox checked={typeName.indexOf(name) > -1} />
-                  <ListItemText primary={name} />
-                </MenuItem>
-              ))}
-            </Select>
-          </Grid>
-          <Grid item md={3} xs={12}>
-            <InputLabel id="cuisine-label">Cuisine</InputLabel>
-            <Select
-              labelId="cuisine-label"
-              id="cuisine"
-              multiple
-              fullWidth
-              value={cuisineName}
-              onChange={handleChangeCuisine}
-              input={<Input />}
-              renderValue={(selected) => selected.join(", ")}
-              MenuProps={MenuProps}
-            >
-              {cuisineNames.map((name) => (
-                <MenuItem key={name} value={name}>
-                  <Checkbox checked={cuisineName.indexOf(name) > -1} />
-                  <ListItemText primary={name} />
-                </MenuItem>
-              ))}
-            </Select>
-          </Grid>
-        </Grid>
-      </Box>
+        </Box>
+      </Card>
+    </Box>
   );
 }
 
