2ceb4cb111e41f304196ec73e3a842bb603d74b5
ary4n99 <ary4n99@gmail.com>
2021-03-05 12:56:09 +0000

    Adds delete account button

--- client/src/views/settings/SettingsView/components/ProfileCard.js
+++ client/src/views/settings/SettingsView/components/ProfileCard.js
@@ -4,6 +4,11 @@ import {
   Card,
   CardContent,
   Typography,
+  Divider,
+  Button,
+  Dialog,
+  DialogContent,
+  DialogContentText,
   makeStyles,
 } from "@material-ui/core";
 import React, { useState } from "react";
@@ -11,18 +16,22 @@ import React, { useState } from "react";
 import { getUserCredentials } from "src/components/auth/UserAuth";
 import PropTypes from "prop-types";
 import clsx from "clsx";
+import { red } from "@material-ui/core/colors";
 
 const useStyles = makeStyles(() => ({
-  root: {},
   avatar: {
     height: 100,
     width: 100,
+  },
+  button: {
+    color: red[500],
   },
 }));
 
 const ProfileCard = ({ className, ...rest }) => {
   const classes = useStyles();
   const [imageURL, setImageURL] = useState("");
+  const [open, setOpen] = React.useState(false);
 
   const [userName, setUserName] = useState(() => {
     getUserCredentials().then((authResponse) => {
@@ -40,7 +49,7 @@ const ProfileCard = ({ className, ...rest }) => {
   });
 
   return (
-    <Card className={clsx(classes.root, className)} {...rest}>
+    <Card {...rest}>
       <CardContent>
         <Box alignItems="center" display="flex" flexDirection="column">
           <Box pb={2}>
@@ -52,14 +61,50 @@ const ProfileCard = ({ className, ...rest }) => {
           <Typography color="textSecondary" variant="body1">
             {""}
           </Typography>
+          <Box pt={2}>
+            <Button
+              color="secondary"
+              variant="text"
+              className={classes.button}
+              onClick={() => {
+                setOpen(true);
+              }}
+            >
+              Delete account
+            </Button>
+          </Box>
         </Box>
       </CardContent>
-      {/* <Divider />
-      <CardActions>
-        <Button color="primary" fullWidth variant="text">
-          Upload picture
-        </Button>
-      </CardActions> */}
+      <Dialog
+        open={open}
+        onClose={() => {
+          setOpen(false);
+        }}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Are you sure you would like to delete your account? This will delete
+            all your data from our databases, including your favourites and
+            dietary preferences.
+            <Box
+              pt={2}
+              alignItems="center"
+              display="flex"
+              flexDirection="column"
+            >
+              <Button
+                color="secondary"
+                variant="text"
+                className={classes.button}
+              >
+                Yes, I'm sure!
+              </Button>
+            </Box>
+          </DialogContentText>
+        </DialogContent>
+      </Dialog>
     </Card>
   );
 };
