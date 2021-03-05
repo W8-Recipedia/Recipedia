88c59801f106f31892d9b0dbcaadd78b6d7c0d00
Shadi Abumattar <40639965+AbumattarSA@users.noreply.github.com>
2021-03-05 13:46:06 +0000

    Cleans up API requests

--- client/src/api/SpoonacularAPI.js
+++ client/src/api/SpoonacularAPI.js
@@ -4,6 +4,8 @@ export const getRecipesComplex = async (
   ingredients,
   intolerances,
   diet,
+  dishtypes,
+  cuisines,
   offset = 0,
   query,
   maxReadyTime
@@ -19,6 +21,8 @@ export const getRecipesComplex = async (
       includeIngredients: ingredients,
       diet: diet || undefined,
       intolerances: intolerances || undefined,
+      type: dishtypes || undefined,
+      cuisine: cuisines || undefined,
       offset,
       query,
       maxReadyTime,
@@ -32,48 +36,6 @@ export const getRandomRecipes = async () => {
     params: {
       apiKey: process.env.REACT_APP_RECIPE_API_KEY,
       number: process.env.REACT_APP_MAX_RECIPE_NUMBER,
-    },
-  });
-};
-
-export const getRecipesByIngredients = async (ingredients) => {
-  let url = "/recipes/findByIngredients";
-  return await Axios.get(url, {
-    params: {
-      apiKey: process.env.REACT_APP_RECIPE_API_KEY,
-      ingredients,
-      number: process.env.REACT_APP_MAX_RECIPE_NUMBER,
-      ranking: 1,
-      ignorePantry: true,
-    },
-  });
-};
-
-export const getIngredients = async (query) => {
-  let url = "/food/ingredients/autocomplete";
-  return await Axios.get(url, {
-    params: {
-      apiKey: process.env.REACT_APP_RECIPE_API_KEY,
-      query,
-      number: process.env.REACT_APP_MAX_AUTOCOMPLETE_NUMBER,
-    },
-  });
-};
-
-export const getRecipeInfoById = async (id) => {
-  let url = `/recipes/${id}/information`;
-  return await Axios.get(url, {
-    params: {
-      apiKey: process.env.REACT_APP_RECIPE_API_KEY,
-    },
-  });
-};
-
-export const getInstructions = async (id) => {
-  let url = `/recipes/${id}/analyzedInstructions`;
-  return await Axios.get(url, {
-    params: {
-      apiKey: process.env.REACT_APP_RECIPE_API_KEY,
     },
   });
 };
