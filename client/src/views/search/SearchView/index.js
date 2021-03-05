28cd07aaa8f7903102e3474d1691515ae3eb32fb
Shadi Abumattar <40639965+AbumattarSA@users.noreply.github.com>
2021-03-05 13:47:10 +0000

    Implements filtering by dish type & cuisine on search page

--- client/src/views/search/SearchView/index.js
+++ client/src/views/search/SearchView/index.js
@@ -1,5 +1,15 @@
 import React, { useEffect, useState } from "react";
-import { Box, Container, makeStyles } from "@material-ui/core";
+import { 
+  Box,
+  Grid, 
+  Container, 
+  makeStyles,
+  Select,
+  InputLabel,
+  Input,
+  Checkbox,
+  MenuItem,
+  ListItemText, } from "@material-ui/core";
 import Page from "src/components/Page";
 // import { getExampleRecipes } from "src/api/mockAPI";
 import { getRecipesComplex } from "src/api/SpoonacularAPI";
@@ -7,6 +17,17 @@ import RecipeInfoDialog from "src/views/search/SearchView/components/RecipeInfoD
 import RecipeCardList from "src/views/search/SearchView/components/RecipeCardList";
 import Searchbar from "src/views/search/SearchView/components/Searchbar";
 import { Scrollbars } from "react-custom-scrollbars";
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -17,6 +38,52 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+const cuisineNames = [
+  "African",
+  "American",
+  "British",
+  "Cajun",
+  "Caribbean",
+  "Chinese",
+  "Eastern European",
+  "European",
+  "French",
+  "German",
+  "Greek",
+  "Indian",
+  "Irish",
+  "Italian",
+  "Japanese",
+  "Jewish",
+  "Korean",
+  "Latin American",
+  "Mediterranean",
+  "Mexican",
+  "Middle Eastern",
+  "Nordic",
+  "Southern",
+  "Spanish",
+  "Thai",
+  "Vietnamese",
+];
+
+const typeNames = [
+  "main course",
+  "side dish",
+  "dessert",
+  "appetizer",
+  "salad",
+  "bread",
+  "breakfast",
+  "soup",
+  "beverage",
+  "sauce",
+  "marinade",
+  "fingerfood",
+  "snack",
+  "drink",
+];
+
 const SearchView = () => {
   const classes = useStyles();
   const [recipes, setRecipes] = useState([]);
@@ -24,14 +91,23 @@ const SearchView = () => {
   const [selectedRecipeInfo, setSelectedRecipeInfo] = useState({});
   const [dlgOpen, setDlgOpen] = useState(false);
   const [searchQuery, setSearchQuery] = useState("");
-  const [offset] = useState(0);
   const [ingredients] = useState([]);
-  const [diets] = useState([]);
   const [intolerances] = useState([]);
+  const [diets] = useState([]);
+  const [cuisineName, setCuisineName] = React.useState([]);
+  const [typeName, setTypeName] = React.useState([]);
+
+  const handleChangeCuisine = (event) => {
+    setCuisineName(event.target.value);
+  };
+
+  const handleChangeType = (event) => {
+    setTypeName(event.target.value);
+  };
 
   const handleQuerySearch = (query) => {
     setSearchQuery(query);
-    loadRecipes([], ingredients, intolerances, 0, query);
+    loadRecipes(ingredients, intolerances, diets, typeName, cuisineName, 0, query);
   };
 
   const onRecipeClick = (id) => {
@@ -48,7 +124,53 @@ const SearchView = () => {
     <Scrollbars>
       <Page className={classes.root} title="Recipedia | Search">
         <Container maxWidth={false}>
-          <Searchbar onSubmit={handleQuerySearch} />
+        <Box mt={1}>
+          <Grid container spacing={3}>
+            <Searchbar onSubmit={handleQuerySearch} />
+            <Grid item md={3} xs={12}>
+              <InputLabel id="type-label">Type</InputLabel>
+              <Select
+                labelId="type-label"
+                id="type"
+                multiple
+                fullWidth
+                value={typeName}
+                onChange={handleChangeType}
+                input={<Input />}
+                renderValue={(selected) => selected.join(", ")}
+                MenuProps={MenuProps}
+              >
+                {typeNames.map((name) => (
+                  <MenuItem key={name} value={name}>
+                    <Checkbox checked={typeName.indexOf(name) > -1} />
+                    <ListItemText primary={name} />
+                  </MenuItem>
+                ))}
+              </Select>
+            </Grid>
+            <Grid item md={3} xs={12}>
+              <InputLabel id="cuisine-label">Cuisine</InputLabel>
+              <Select
+                labelId="cuisine-label"
+                id="cuisine"
+                multiple
+                fullWidth
+                value={cuisineName}
+                onChange={handleChangeCuisine}
+                input={<Input />}
+                renderValue={(selected) => selected.join(", ")}
+                MenuProps={MenuProps}
+              >
+                {cuisineNames.map((name) => (
+                  <MenuItem key={name} value={name}>
+                    <Checkbox checked={cuisineName.indexOf(name) > -1} />
+                    <ListItemText primary={name} />
+                  </MenuItem>
+                ))}
+              </Select>
+            </Grid>
+          </Grid>
+        </Box>        
           <Box mt={3}>
             <RecipeCardList recipes={recipes} onRecipeClick={onRecipeClick} />
           </Box>
@@ -67,16 +189,22 @@ const SearchView = () => {
     ingredientsArray,
     intolerancesArray,
     dietsArray,
+    typesArray,
+    cuisineArray,
     offset,
     query
   ) {
     let ingredientsString = ingredientsArray.map((o) => o.name).join(",");
     let intolerancesString = intolerancesArray.join(",");
     let dietsString = dietsArray.join(",");
+    let dishTypesString = typesArray.join(",");
+    let cuisinesString = cuisineArray.join(",");
     getRecipesComplex(
       ingredientsString,
       intolerancesString,
       dietsString,
+      dishTypesString,
+      cuisinesString,
       offset,
       query
     )
