a197fd1db27cea718f7ddc05b1b99d7c4abab360
Aryan Agrawal <ary4n99@gmail.com>
2021-03-14 00:12:08 +0000

    Fixes authcheck to match server responses

--- client/src/components/auth/AuthCheck.js
+++ client/src/components/auth/AuthCheck.js
@@ -1,23 +1,26 @@
-import { useNavigate } from "react-router-dom";
-import React, { useState } from "react";
-import { getUserInfo } from "src/components/auth/UserAuth";
+import React, { useLayoutEffect, useState } from "react";
+
+import DashboardView from "src/views/dashboard/DashboardView";
 import LandingView from "src/views/landing/LandingView";
 import LoginView from "src/views/login/LoginView";
-import DashboardView from "src/views/dashboard/DashboardView";
+import { getUserData } from "src/components/auth/UserAuth";
+import { useNavigate } from "react-router-dom";
 
 export const LoginCheck = () => {
   const navigate = useNavigate();
-  const pathname = window.location.pathname;
-  const [isloggedIn, setIsLoggedIn] = useState(() => {
-    getUserInfo().then((authResponse) => {
-      setIsLoggedIn(authResponse.data.loggedIn);
-      if (authResponse.data.loggedIn) {
+  const [isloggedIn, setIsLoggedIn] = useState(false);
+
+  useLayoutEffect(() => {
+    getUserData().then((response) => {
+      if (response.data.message === "loggedIn") {
+        setIsLoggedIn(true);
         navigate("/app/home");
       }
     });
   });
+
   return isloggedIn === false ? (
-    pathname === "/login" ? (
+    window.location.pathname === "/login" ? (
       <LoginView />
     ) : (
       <LandingView />
@@ -27,10 +30,12 @@ export const LoginCheck = () => {
 
 export const AccessCheck = () => {
   const navigate = useNavigate();
-  const [isloggedIn, setIsLoggedIn] = useState(() => {
-    getUserInfo().then((authResponse) => {
-      setIsLoggedIn(authResponse.data.loggedIn);
-      if (!authResponse.data.loggedIn) {
+  const [isloggedIn, setIsLoggedIn] = useState(true);
+
+  useLayoutEffect(() => {
+    getUserData().then((response) => {
+      if (response.data.message != "loggedIn") {
+        setIsLoggedIn(false);
         navigate("/");
       }
     });
