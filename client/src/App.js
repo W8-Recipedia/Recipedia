6d1bac0d3b60bda0ebb83a9683c3371dcb22aaf2
Shadi Abumattar <40639965+AbumattarSA@users.noreply.github.com>
2021-03-15 23:54:53 +0000

    Adds SnackbarProvider

--- client/src/App.js
+++ client/src/App.js
@@ -14,51 +14,63 @@ import SettingsView from "src/views/settings/SettingsView";
 import SignUpView from "src/views/signup/SignUpView";
 import Styles from "src/components/theme/styles";
 import Theme from "src/components/theme";
-import { ThemeProvider } from "@material-ui/core";
+import { Button, ThemeProvider } from "@material-ui/core";
+import { SnackbarProvider } from "notistack";
 import VerifyView from "src/views/verify/VerifyView";
 
 const App = () => {
+  const notistackRef = React.createRef();
   const [theme, setTheme] = useState(Theme.light);
+  const onClickDismiss = (key) => () => {
+    notistackRef.current.closeSnackbar(key);
+  };
 
   return (
     <ThemeProvider theme={theme}>
-      <Styles />
-      {useRoutes([
-        {
-          path: "app",
-          element: (
-            <AccessCheck
-              toggleDarkMode={() => {
-                theme === Theme.light
-                  ? setTheme(Theme.dark)
-                  : setTheme(Theme.light);
-              }}
-            />
-          ),
-          children: [
-            { path: "home/*", element: <HomeView /> },
-            { path: "search/*", element: <SearchView /> },
-            { path: "favourites/*", element: <FavouritesView /> },
-            { path: "settings", element: <SettingsView /> },
-            { path: "faq", element: <FAQView /> },
-            { path: "feedback", element: <FeedbackView /> },
-            { path: "legal", element: <LegalView /> },
-            { path: "", element: <Navigate to="/app/home" /> },
-            { path: "*", element: <Navigate to="/404" /> },
-          ],
-        },
-        {
-          path: "/",
-          children: [
-            { path: "signup", element: <SignUpView /> },
-            { path: "login", element: <LoginCheck /> },
-            { path: "legal", element: <LegalView /> },
-            { path: "verify/*", element: <VerifyView /> },
-            { path: "", element: <LoginCheck /> },
-            { path: "*", element: <NotFoundView /> },
-          ],
-        },
-      ])}
+      <SnackbarProvider
+        ref={notistackRef}
+        action={(key) => <Button onClick={onClickDismiss(key)}>Dismiss</Button>}
+        maxSnack={2}
+        dense
+      >
+        <Styles />
+        {useRoutes([
+          {
+            path: "app",
+            element: (
+              <AccessCheck
+                toggleDarkMode={() => {
+                  theme === Theme.light
+                    ? setTheme(Theme.dark)
+                    : setTheme(Theme.light);
+                }}
+              />
+            ),
+            children: [
+              { path: "home/*", element: <HomeView /> },
+              { path: "search/*", element: <SearchView /> },
+              { path: "favourites/*", element: <FavouritesView /> },
+              { path: "settings", element: <SettingsView /> },
+              { path: "faq", element: <FAQView /> },
+              { path: "feedback", element: <FeedbackView /> },
+              { path: "legal", element: <LegalView /> },
+              { path: "", element: <Navigate to="/app/home" /> },
+              { path: "*", element: <Navigate to="/404" /> },
+            ],
+          },
+          {
+            path: "/",
+            children: [
+              { path: "signup", element: <SignUpView /> },
+              { path: "login", element: <LoginCheck /> },
+              { path: "legal", element: <LegalView /> },
+              { path: "verify/*", element: <VerifyView /> },
+              { path: "", element: <LoginCheck /> },
+              { path: "*", element: <NotFoundView /> },
+            ],
+          },
+        ])}
+      </SnackbarProvider>
     </ThemeProvider>
   );
 };
