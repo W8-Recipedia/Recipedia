b2997d3ca1214696b113a897dfe12939ae37c940
Aryan Agrawal <ary4n99@gmail.com>
2021-03-16 13:45:55 +0000

    Optimizes landing page

--- client/src/views/landing/LandingView/index.js
+++ client/src/views/landing/LandingView/index.js
@@ -1,25 +1,180 @@
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import { CssBaseline } from "@material-ui/core";
-import Header from "./components/Header";
-import PlaceToVisit from "./components/functionalities";
-// import BackgroundHeader from "./src/views/landing/LandingView/assets/bg.png";
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  CardMedia,
+  Container,
+  Grid,
+  Hidden,
+  IconButton,
+  Typography,
+  makeStyles,
+} from "@material-ui/core";
+import React, { useEffect, useRef, useState } from "react";
+
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import { Link } from "react-router-dom";
+import Page from "src/components/theme/page";
+import { Scrollbars } from "react-custom-scrollbars";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    minHeight: "100vh",
-    backgroundImage: `url(${"/assets/bg.png"})`,
-    backgroundRepeat: "no-repeat",
-    backgroundSize: "cover",
+    backgroundColor: theme.palette.background.dark,
+    height: "100%",
+    paddingBottom: theme.spacing(3),
+    paddingTop: theme.spacing(3),
+  },
+  title: {
+    fontSize: 84,
+    [theme.breakpoints.up("xs")]: {
+      fontSize: 64,
+    },
+    [theme.breakpoints.up("md")]: {
+      fontSize: 84,
+    },
+  },
+  buttonText: {
+    fontSize: 24,
+  },
+  goDown: {
+    fontSize: "4rem",
+  },
+  image: {
+    width: "100%",
+    transition: "opacity 250ms ease",
+    backfaceVisibility: "hidden",
   },
 }));
-export default function App() {
+
+const LandingView = () => {
+  const cardRef = useRef(null);
+
   const classes = useStyles();
+  const [checked, setChecked] = useState(false);
+  useEffect(() => {
+    setChecked(true);
+  }, []);
   return (
-    <div className={classes.root}>
-      <CssBaseline />
-      <Header />
-      <PlaceToVisit />
-    </div>
+    <Scrollbars>
+      <Page className={classes.root} title="Recipedia">
+        <Box
+          display="flex"
+          flexDirection="column"
+          height="50%"
+          justifyContent="center"
+        >
+          <Container maxWidth="md">
+            <Typography align="center" color="textSecondary" variant="h2">
+              Eat smarter with
+            </Typography>
+            <Typography
+              align="center"
+              color="textPrimary"
+              variant="h1"
+              className={classes.title}
+            >
+              Recipedia
+            </Typography>
+          </Container>
+        </Box>
+        <Box>
+          <Container maxWidth="sm">
+            <Grid container spacing={3}>
+              <Grid item xs={12} md={6}>
+                <Link to="/signup">
+                  <Button
+                    color="primary"
+                    fullWidth
+                    variant="contained"
+                    size="large"
+                    className={classes.buttonText}
+                  >
+                    Sign Up
+                  </Button>
+                </Link>
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <Link to="/login">
+                  <Button
+                    color="primary"
+                    fullWidth
+                    variant="contained"
+                    size="large"
+                    className={classes.buttonText}
+                  >
+                    Log in
+                  </Button>
+                </Link>
+              </Grid>
+              <Grid item xs={12} md={12}>
+                <Box display="flex" justifyContent="center">
+                  <IconButton>
+                    <ExpandMoreIcon
+                      className={classes.goDown}
+                      onClick={() =>
+                        cardRef.current.scrollIntoView({
+                          behavior: "smooth",
+                          block: "start",
+                        })
+                      }
+                    />
+                  </IconButton>
+                </Box>
+              </Grid>
+            </Grid>
+          </Container>
+        </Box>
+        <Hidden only={["xs", "sm", "md"]}>
+          <Box pt={5}>
+            <Grid container>
+              <Grid item md={6} xs={12}>
+                <Box mt={20} ml={10} mr={10}>
+                  <Card ref={cardRef}>
+                    <CardContent>
+                      <Typography gutterBottom variant="h2">
+                        Favourites
+                      </Typography>
+                      <Typography variant="body2" color="textSecondary">
+                        With the Favorite functionality users will be able to
+                        save recipies that they discovered using Recipedia and
+                        wish to keep them saved to be quickly retrieved
+                      </Typography>
+                    </CardContent>
+                    <img
+                      className={classes.image}
+                      src="https://images.unsplash.com/photo-1579546929662-711aa81148cf?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHw%3D&w=1000&q=80"
+                    />
+                  </Card>
+                </Box>
+              </Grid>
+              <Grid item md={6} xs={12}>
+                <Box mb={20} mt={20} ml={10} mr={10}>
+                  <Card>
+                    <CardContent>
+                      <Typography gutterBottom variant="h2">
+                        Search
+                      </Typography>
+                      <Typography variant="body2" color="textSecondary">
+                        Within the Recipedia search functionality one can
+                        specify their dietary requirements by selecting from an
+                        extensive list of options that include but are not
+                        limited to.
+                      </Typography>
+                    </CardContent>
+                    <img
+                      className={classes.image}
+                      src="https://images.unsplash.com/photo-1579546929662-711aa81148cf?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHw%3D&w=1000&q=80"
+                    />
+                  </Card>
+                </Box>
+              </Grid>
+            </Grid>
+          </Box>
+        </Hidden>
+      </Page>
+    </Scrollbars>
   );
-}
+};
+
+export default LandingView;
