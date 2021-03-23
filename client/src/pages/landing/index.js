5f2e1442fe6cb977cedd16b9a20a4c3b24568d2a
Aryan Agrawal <ary4n99@gmail.com>
2021-03-23 19:23:23 +0000

    Removes redundant code and improves cards

--- client/src/pages/landing/index.js
+++ client/src/pages/landing/index.js
@@ -1,55 +1,50 @@
+import { Avatar, IconButton, Toolbar } from "@material-ui/core";
+import { Moon, Sun } from "react-feather";
 import { React, useState } from "react";
-import AppBar from "@material-ui/core/AppBar";
-import Button from "@material-ui/core/Button";
-import Card from "@material-ui/core/Card";
-import CardContent from "@material-ui/core/CardContent";
-import Grid from "@material-ui/core/Grid";
-import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles";
-import Container from "@material-ui/core/Container";
-import Link from "@material-ui/core/Link";
-import { Link as RouterLink } from "react-router-dom";
-import Box from "@material-ui/core/Box";
-import Page from "src/components/theme/page";
-import { Scrollbars } from "react-custom-scrollbars";
 import {
   ThemeProvider,
   createMuiTheme,
   responsiveFontSizes,
 } from "@material-ui/core/styles";
-import { Toolbar, IconButton, Avatar } from "@material-ui/core";
-import { Moon, Sun } from "react-feather";
-import RestaurantIcon from "@material-ui/icons/Restaurant";
+
+import AppBar from "@material-ui/core/AppBar";
+import Box from "@material-ui/core/Box";
 import BrushIcon from "@material-ui/icons/Brush";
+import Button from "@material-ui/core/Button";
+import Card from "@material-ui/core/Card";
+import Container from "@material-ui/core/Container";
+import EmojiObjectsIcon from "@material-ui/icons/EmojiObjects";
+import Grid from "@material-ui/core/Grid";
+import Link from "@material-ui/core/Link";
+import Page from "src/components/theme/page";
+import RestaurantIcon from "@material-ui/icons/Restaurant";
+import { Link as RouterLink } from "react-router-dom";
+import SaveIcon from "@material-ui/icons/Save";
+import { Scrollbars } from "react-custom-scrollbars";
+import SearchIcon from "@material-ui/icons/Search";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
 
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {"Copyright © "}
-      <Link color="inherit" href="https://material-ui.com/">
-        Recipedia
-      </Link>{" "}
-      {new Date().getFullYear()}
-    </Typography>
-  );
-}
-
-let theme = createMuiTheme({
-  typography: {
-    overline: {
-      fontWeight: 400,
-      fontSize: 20,
-      paddingLeft: 10,
+const theme = responsiveFontSizes(
+  createMuiTheme({
+    typography: {
+      overline: {
+        fontWeight: 400,
+        fontSize: 20,
+        paddingLeft: 10,
+      },
     },
-  },
-});
-theme = responsiveFontSizes(theme);
+  })
+);
 
 const useStyles = makeStyles((theme) => ({
   root: {
     backgroundColor: theme.palette.background.dark,
   },
-  title: {
+  topBarTitle: {
+    flexGrow: 1,
+  },
+  heroTitle: {
     fontSize: 84,
     [theme.breakpoints.up("xs")]: {
       fontSize: 64,
@@ -58,44 +53,23 @@ const useStyles = makeStyles((theme) => ({
       fontSize: 84,
     },
   },
-  topBarTitle: {
-    flexGrow: 1,
-  },
-  icon: {
-    marginRight: theme.spacing(2),
+  avatar: {
+    height: 100,
+    width: 100,
   },
   heroContent: {
     backgroundColor: theme.palette.background.paper,
   },
-  cardGrid: {
-    paddingTop: theme.spacing(8),
-    paddingBottom: theme.spacing(8),
-  },
-  card: {
-    height: "100%",
-    display: "flex",
-    flexDirection: "column",
-  },
-  cardMedia: {
-    paddingTop: "56.25%", // 16:9
-  },
-  cardContent: {
-    flexGrow: 1,
+  buttonText: {
+    fontSize: 24,
   },
   footer: {
     backgroundColor: theme.palette.background.paper,
     padding: theme.spacing(4),
   },
-  avatar: {
-    width: 60,
-    height: 60,
-  },
-  buttonText: {
-    fontSize: 24,
-  },
 }));
 
-export default function LandingView({ toggleDarkMode }) {
+const LandingView = ({ toggleDarkMode }) => {
   const classes = useStyles();
   const [darkMode, setDarkMode] = useState(false);
 
@@ -114,7 +88,6 @@ export default function LandingView({ toggleDarkMode }) {
                 Recipedia
               </Typography>
             </ThemeProvider>
-
             <Box>
               <IconButton
                 color="inherit"
@@ -144,7 +117,7 @@ export default function LandingView({ toggleDarkMode }) {
                   align="center"
                   color="textPrimary"
                   variant="h1"
-                  className={classes.title}
+                  className={classes.heroTitle}
                   gutterBottom
                 >
                   Recipedia
@@ -192,76 +165,111 @@ export default function LandingView({ toggleDarkMode }) {
             </Grid>
           </Container>
         </Box>
-        <Container className={classes.cardGrid}>
+        <Box m={10} mt={12} mb={12}>
           <Grid container spacing={2}>
             <Grid item xs={12} sm={6} md={3}>
-              <Card className={classes.card}>
-                <Avatar>
-                  <BrushIcon />
-                </Avatar>
-                <CardContent className={classes.cardContent}>
-                  <Typography gutterBottom variant="h2" component="h2">
-                    Easy customization
-                  </Typography>
-                  {/* <Typography>
-                    You can (optionally) specify your allergens, any diet you
-                    follow, and even calculate your TDEE and BMI to find the
-                    optimal number of calories to consume at each meal!
-                  </Typography> */}
-                </CardContent>
-              </Card>
-            </Grid>
-            <Grid item xs={12} sm={6} md={3}>
-              <Card className={classes.card}>
-                <CardContent className={classes.cardContent}>
-                  <Typography gutterBottom variant="h2" component="h2">
-                    Unbounded search
-                  </Typography>
-                  <Typography>
-                    Within search, you can specify cuisines and dish types, to
-                    find the recipes you really want to eat! All of the search
-                    results are guaranteed to adhere to the dietary
-                    restrictions/allergens you have, given that you specify them
-                    in the settings page.
-                  </Typography>
-                </CardContent>
-              </Card>
-            </Grid>
-            <Grid item xs={12} sm={6} md={3}>
-              <Card className={classes.card}>
-                <CardContent className={classes.cardContent}>
-                  <Typography gutterBottom variant="h2" component="h2">
-                    Recipe saving
-                  </Typography>
-                  <Typography>
-                    With the favourites functionality, users are able to save
-                    their tastiest recipes found, for quick and easy access in
-                    the future. You can favourite as many recipes as you like,
-                    and the more recipes you favourite, the higher you level up
-                  </Typography>
-                </CardContent>
-              </Card>
-            </Grid>
-            <Grid item xs={12} sm={6} md={3}>
-              <Card className={classes.card}>
-                <CardContent className={classes.cardContent}>
-                  <Typography gutterBottom variant="h2" component="h2">
+              <Card>
+                <Box p={2}>
+                  <Box display="flex" justifyContent="center" pb={2}>
+                    <Avatar className={classes.avatar}>
+                      <EmojiObjectsIcon style={{ fontSize: 50 }} />
+                    </Avatar>
+                  </Box>
+                  <Typography
+                    gutterBottom
+                    variant="h3"
+                    align="center"
+                  >
                     Smart recommendations
                   </Typography>
-                  <Typography>
-                    Recipedia uses a propriety algorithm to find and recommend
-                    the best recipes for you and your fitness goals, based on
-                    all the (optional) data given to us directly by you.
+
+                  <Typography align="center">
+                    Get personalized recommendations for your health &amp; goals.
                   </Typography>
-                </CardContent>
+                </Box>
+              </Card>
+            </Grid>
+            <Grid item xs={12} sm={6} md={3}>
+              <Card>
+                <Box p={2}>
+                  <Box display="flex" justifyContent="center" pb={2}>
+                    <Avatar className={classes.avatar}>
+                      <BrushIcon style={{ fontSize: 50 }} />
+                    </Avatar>
+                  </Box>
+                  <Typography
+                    gutterBottom
+                    variant="h3"
+                    align="center"
+                  >
+                    Easy customization
+                  </Typography>
+                  <Typography align="center">
+                    Specify your allergens, diet, and health data to find
+                    the best meals for you!
+                  </Typography>
+                </Box>
+              </Card>
+            </Grid>
+            <Grid item xs={12} sm={6} md={3}>
+              <Card>
+                <Box p={2}>
+                  <Box display="flex" justifyContent="center" pb={2}>
+                    <Avatar className={classes.avatar}>
+                      <SearchIcon style={{ fontSize: 50 }} />
+                    </Avatar>
+                  </Box>
+                  <Typography
+                    gutterBottom
+                    variant="h3"
+                    align="center"
+                  >
+                    Unbounded search
+                  </Typography>
+
+                  <Typography align="center">
+                    Filter by cuisines and dish types to find the recipes you
+                    really want to eat!
+                  </Typography>
+                </Box>
+              </Card>
+            </Grid>
+            <Grid item xs={12} sm={6} md={3}>
+              <Card>
+                <Box p={2}>
+                  <Box display="flex" justifyContent="center" pb={2}>
+                    <Avatar className={classes.avatar}>
+                      <SaveIcon style={{ fontSize: 50 }} />
+                    </Avatar>
+                  </Box>
+                  <Typography
+                    gutterBottom
+                    variant="h3"
+                    align="center"
+                  >
+                    Recipe saving
+                  </Typography>
+                  <Typography align="center">
+                    Save your favourite recipes for quick and easy access in the
+                    future.
+                  </Typography>
+                </Box>
               </Card>
             </Grid>
           </Grid>
-        </Container>
+        </Box>
         <Box className={classes.footer}>
-          <Copyright />
+          <Typography variant="body2" color="textSecondary" align="center">
+            {"Copyright © "}
+            <Link color="inherit" href="https://material-ui.com/">
+              Recipedia
+            </Link>{" "}
+            {new Date().getFullYear()}
+          </Typography>
         </Box>
       </Page>
     </Scrollbars>
   );
-}
+};
+
+export default LandingView;
